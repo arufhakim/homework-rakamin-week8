3acfd91addb1ad599f299782343d0c1da883fbf0
arufhakim <arufhakim@gmail.com>
2023-02-21 19:03:03 +0700

    update error

--- src/route.js
+++ src/route.js
@@ -20,7 +20,7 @@ router.get('/film', (req, res) => {
     const getFilm = 'SELECT * FROM film ORDER BY film_id ASC';
     pool.query(getFilm, (err, result) => {
         if (err) {
-            console.log(err);
+            console.log(err.message);
         } else {
             res.status(200).json(result.rows).end();
         }
@@ -31,7 +31,7 @@ router.get('/film/:id', (req, res) => {
     const getFilmById = `SELECT * FROM film WHERE film_id = ${req.params.id}`;
     pool.query(getFilmById, (err, result) => {
         if (err) {
-            console.log(err);
+            console.log(err.message);
         } else {
             res.status(200).json(result.rows).end();
         }
@@ -42,7 +42,7 @@ router.get('/category', (req, res) => {
     const getCategory = 'SELECT * FROM category';
     pool.query(getCategory, (err, result) => {
         if (err) {
-            console.log(err);
+            console.log(err.message);
         } else {
             res.status(200).json(result.rows).end();
         }
@@ -53,7 +53,7 @@ router.get('/category/:id/film', (req, res) => {
     const getFilmByCategory = `SELECT * FROM film f LEFT JOIN film_category fc ON f.film_id = fc.film_id RIGHT JOIN category c ON fc.category_id = c.category_id WHERE fc.category_id = ${req.params.id}`;
     pool.query(getFilmByCategory, (err, result) => {
         if (err) {
-            console.log(err);
+            console.log(err.message);
         } else {
             console.log(`Banyak Film pada Category ${req.params.id} adalah ${result.rows.length}`);
             res.status(200).json(result.rows).end();
